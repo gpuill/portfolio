bf0535d880d47eacf50f43b7da0b85ccf4c44979
Rochpearse <guillaume.puill@icloud.com>
2025-01-03 18:32:18 +0100

    YES

--- pages/projets/index.vue
+++ pages/projets/index.vue
@@ -4,6 +4,7 @@
             <h1>Mes Projets</h1>
             <div class="project-container">
                 <div v-for="projet in projets" :key="projet.id" class="project-row">
+
                     <project-card :formation="projet.formation" :annee="projet.annee" :nom="projet.nom"
                         :github="projet.github" :resume="projet.resume" :technologie="projet.technologie"
                         :mainColor="projet['main-color']" :secondaryColor="projet['secondary-color']" :logo="projet.logo" />
