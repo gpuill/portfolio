809146dc85d5f39c300c43838ccf9c0d64c4e52f
Rochpearse <guillaume.puill@icloud.com>
2025-01-10 13:24:58 +0100

    🚑 COSMIC GIIIIIRL 🚑

--- server/api/getLastCommit.get.ts
+++ server/api/getLastCommit.get.ts
@@ -3,16 +3,20 @@ export default defineEventHandler(async (event) => {
 
     const config = useRuntimeConfig(event);
     const urlGetMaster = "https://api.github.com/repos/gpuill/portfolio/git/refs/heads/master";
+    const headers = getRequestHeaders(event)
+    console.log(headers.Host)
+    console.log(headers)
 
     const config1 = {
         method: "GET",
+        Headers:headers
     } as any
     try {
-        const jsonMaster = await $fetch(urlGetMaster, config1) as any;
-        return "toto";
+        const jsonMaster:JSON = await $fetch(urlGetMaster, config1) as any;
+        return jsonMaster;
     } catch (error) {
 
-        return config1
+        return error
     }
         
 
