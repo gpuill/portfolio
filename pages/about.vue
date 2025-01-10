fae0ae5fe04defd325fe0e1262e8c2487553d5a6
Rochpearse <guillaume.puill@icloud.com>
2025-01-10 14:08:06 +0100

    🚑

--- pages/about.vue
+++ pages/about.vue
@@ -3,9 +3,41 @@
         <p>
             C'est moi🚀
         </p>
-        <last-commit-portfolio/>
+        <pre>
+            {{ lastCommit }}
+        </pre>
+
     </main>
 </template>
-<script setup>
 
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+// Variables et URLs
+const lastCommit = ref(null);
+
+const fetchLastCommit = async () => {
+    try {
+        
+        const commit = await $fetch("https://api.github.com/repos/gpuill/portfolio/git/refs/heads/master", {
+            method: "GET",
+        });
+
+        // Récupérer les informations du dernier commit
+        if (commit) {
+            lastCommit.value = `🚑 ${commit} 🚑`;
+        } else {
+            lastCommit.value = "Aucun commit trouvé.";
+        }
+    } catch (error) {
+        console.error("Erreur lors de la récupération des commits :", error);
+        lastCommit.value = "Une erreur est survenue lors de la récupération du commit.";
+    }
+};
+
+// Appeler la fonction lors du montage du composant
+onMounted(() => {
+    fetchLastCommit();
+});
 </script>
