5ad14f898b7fbe8f90eee5931a1b99c04aaebd9c
Rochpearse <guillaume.puill@icloud.com>
2025-01-10 10:48:20 +0100

    🧐

--- server/api/getLastCommit.get.ts
+++ server/api/getLastCommit.get.ts
@@ -1,7 +1,5 @@
-import { useLogger } from "nuxt/kit";
 
 export default defineEventHandler(async (event) => {
-    const logger = useLogger('API-getLastCommit')
 
     const config = useRuntimeConfig(event);
     const urlGetMaster = "https://api.github.com/repos/gpuill/portfolio/git/refs/heads/master";
@@ -15,9 +13,7 @@ export default defineEventHandler(async (event) => {
             
         },
     } as any
-    logger.info(JSON.stringify(process.env))
-    logger.info(JSON.stringify(config1))
     console.log(process.env)
 
-    return config;
+    return process.env;
 });
