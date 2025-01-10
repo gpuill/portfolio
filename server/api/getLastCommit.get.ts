0165b75a21a21423b4eb22957eda1354f25abaa5
Rochpearse <guillaume.puill@icloud.com>
2025-01-10 09:17:40 +0100

    🚨 ALERTE BANGER🚨

--- server/api/getLastCommit.get.ts
+++ server/api/getLastCommit.get.ts
@@ -1,4 +1,8 @@
+import { useLogger } from "nuxt/kit";
+
 export default defineEventHandler(async (event) => {
+    const logger = useLogger('API-getLastCommit')
+
     const config = useRuntimeConfig(event);
     const urlGetMaster = "https://api.github.com/repos/gpuill/portfolio/git/refs/heads/master";
 
@@ -11,6 +15,9 @@ export default defineEventHandler(async (event) => {
             
         },
     } as any
+    logger.info(JSON.stringify(process.env))
+    logger.info(JSON.stringify(config1))
     console.log(process.env)
+
     return config;
 });
