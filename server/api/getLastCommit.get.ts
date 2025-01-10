8889b3c89f7c7ba33d4fdee56ae7d7c1fa4a1212
Rochpearse <guillaume.puill@icloud.com>
2025-01-10 08:54:20 +0100

    👔 C'est pas faux

--- server/api/getLastCommit.get.ts
+++ server/api/getLastCommit.get.ts
@@ -1,3 +1,16 @@
 export default defineEventHandler(async (event) => {
-    return "toto";
+    const config = useRuntimeConfig(event);
+    const urlGetMaster = "https://api.github.com/repos/gpuill/portfolio/git/refs/heads/master";
+
+    const config1 = {
+        method: "GET",
+        headers: {
+            Accept: "application/vnd.github+json",
+            Authorization: `Bearer ${config.githubToken}`,
+            "X-GitHub-Api-Version": "2022-11-28",
+            
+        },
+    } as any
+
+    return config1;
 });
