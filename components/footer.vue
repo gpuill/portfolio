5b1963d78e9bf07207f13cfddc92cf9339bc923e
Rochpearse <guillaume.puill@icloud.com>
2025-10-29 11:39:49 +0100

    Pouic

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,9 @@
 <template>
-    <footer class="bg-zinc-100">
-        <last-commit/>
+    <footer class="">
     </footer>
 </template>
+
 <script>
 
 </script>
-<style lang="scss">
-@use "~/assets/scss/components/footer.scss"
-</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
     <main id="accueil" class="flex flex-col">
+        <ColorBends class-name="fond" style="" :colors="['#ff5c7a', '#8a5cff', '#00ffd1']" :rotation="0" :speed="0.3"
+            :scale="0.2" :frequency="1.7" :warpStrength="1" :mouseInfluence="0.8" :parallax="0.5" :noise="0.08"
+            transparent />
         <section id="section-intro">
             <h2>Portfolio</h2>
             <h1>Guillaume Puill</h1>
-            <h3>Pour Guillaume Bébou et Johan Bébou qu'ils arrêtent de me broyer les couilles avec ce portfolio de mort</h3>
+            <h3>Pour Guillaume Bébou et Johan Bébou qu'ils arrêtent de me broyer les couilles avec ce portfolio de
+                mort
+            </h3>
 
         </section>
 
@@ -12,10 +17,14 @@
 </template>
 
 <script setup>
-
+import ColorBends from '@/components/vue-bits/ColorBends.vue'
 
 </script>
 
 <style>
-
+.fond {
+    position: absolute;
+    top: 0;
+    height: 100%;
+}
 </style>
